88cd61924e3ae9f41a99ccc607882c7c51ced467
m1nak0v <46508387+mkovace5@users.noreply.github.com>
2024-01-23 15:42:41 +0000

    Update Intro.js

--- src/Pages/Intro/Intro.js
+++ src/Pages/Intro/Intro.js
@@ -10,7 +10,7 @@ function Intro(){
             <div className='text-container'>
                 <h3>HELLO THERE!</h3>
                 <h1>I'M MINA.</h1>
-                <p>I’M A SOFTWARE DEVELOPER AND UX DESIGNER </p>
+                <p>I’M A UX DESIGNER AND SOFTWARE DEVELOPER WHO LOVES TO CREATE USER-CENTRIC, ENGAGING, AND INTUITIVE DESIGNS</p>
             </div>
             <img src={mina} alt="Mina outdoorsy surf picture"/>
         </div>
